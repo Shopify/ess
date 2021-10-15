fec58f0a96797f7cb1c54632c1dfae7e75021f71
Jacob Steves <jacob.steves@shopify.com>
2021-10-15 15:23:30 -0400

    store execution time on mruby engine

--- ext/enterprise_script_service/timer.hpp
+++ ext/enterprise_script_service/timer.hpp
@@ -20,6 +20,8 @@ struct timer {
     std::uint64_t cpu_time_scale_, base_;
     std::function<void(const std::string, const std::int64_t)> writer_;
 
+    std::int64_t get_elapsed_time_us();
+
     scope(const std::string name, const timer &t);
     scope(scope &&rhs) = default;
     scope(const scope &) = delete;
