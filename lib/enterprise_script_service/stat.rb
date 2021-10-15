54bd483b0b40e55b4ee724dbfb6a1b558f193737
Jacob Steves <jacob.steves@shopify.com>
2021-10-15 15:40:15 -0400

    add execution_time_us to stat object

--- lib/enterprise_script_service/stat.rb
+++ lib/enterprise_script_service/stat.rb
@@ -1,7 +1,21 @@
 module EnterpriseScriptService
-  Stat = Struct.new(:instructions, :memory, :bytes_in, :time, :total_instructions) do
+  Stat = Struct.new(
+    :instructions,
+    :memory,
+    :bytes_in,
+    :time,
+    :execution_time_us,
+    :total_instructions
+  ) do
     def initialize(options)
-      super(options[:instructions], options[:memory], options[:bytes_in], options[:time], options[:total_instructions])
+      super(
+        options[:instructions],
+        options[:memory],
+        options[:bytes_in],
+        options[:time],
+        options[:execution_time_us],
+        options[:total_instructions]
+      )
     end
   end
 
@@ -10,6 +24,7 @@ module EnterpriseScriptService
     memory: 0,
     bytes_in: 0,
     time: 0,
+    execution_time_us: 0,
     total_instructions: 0,
   )
 end
--- spec/enterprise_script_service/stat_spec.rb
+++ spec/enterprise_script_service/stat_spec.rb
@@ -2,13 +2,13 @@ RSpec.describe(EnterpriseScriptService::Stat) do
   let(:null_stat) { EnterpriseScriptService::Stat::Null }
 
   it "supports all stats" do
-    options = {instructions: 1, memory: 2, bytes_in: 3, time: 4, total_instructions: 5}
+    options = {instructions: 1, memory: 2, bytes_in: 3, time: 4, execution_time_us: 5, total_instructions: 6}
     stat = EnterpriseScriptService::Stat.new(options)
     expect(stat).to have_attributes(options)
   end
 
   it "nullStats are all zero" do
-    default_values = {instructions: 0, memory: 0, bytes_in: 0, time: 0, total_instructions: 0}
+    default_values = {instructions: 0, memory: 0, bytes_in: 0, time: 0, execution_time_us: 0, total_instructions: 0}
     expect(null_stat).to have_attributes(default_values)
   end
 
